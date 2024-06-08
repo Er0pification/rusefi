9818af223488e495d70eb5e21bb95bded6dba178
Er0pification <97910214+Er0pification@users.noreply.github.com>
2024-06-08 15:01:45 +0300

    Update efifeatures.h

--- firmware/config/stm32f4ems/efifeatures.h
+++ firmware/config/stm32f4ems/efifeatures.h
@@ -95,7 +95,7 @@
 #endif
 
 #ifndef EFI_HPFP
-#define EFI_HPFP TRUE
+#define EFI_HPFP FALSE
 #endif
 
 #ifndef EFI_HD_ACR
@@ -180,7 +180,7 @@
 #endif
 
 #ifndef EFI_VVT_PID
-#define EFI_VVT_PID TRUE
+#define EFI_VVT_PID FALSE
 #endif
 
 #define EFI_SIGNAL_EXECUTOR_SLEEP FALSE
@@ -337,7 +337,7 @@
 #endif
 
 #ifndef EFI_EMBED_INI_MSD
-#define EFI_EMBED_INI_MSD TRUE
+#define EFI_EMBED_INI_MSD FALSE
 #endif
 
 #ifndef EFI_USB_SERIAL
